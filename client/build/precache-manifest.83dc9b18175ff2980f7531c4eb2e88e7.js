2b03dcc9bb59974cebcee963108c92d4224e7b22
Nicholas Jozefczyk <nickjay@Nicholass-MacBook-Air.local>
2018-10-14 23:08:12 -0700

    trying

---
+++ client/build/precache-manifest.83dc9b18175ff2980f7531c4eb2e88e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f2db12973e04a55d02b",
+    "url": "/static/js/main.4f2db129.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4f2db12973e04a55d02b",
+    "url": "/static/css/main.54133193.chunk.css"
+  },
+  {
+    "revision": "4b76f71d0f1146a142cc9519e362f996",
+    "url": "/index.html"
+  }
+];
